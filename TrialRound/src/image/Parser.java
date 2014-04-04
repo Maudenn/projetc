9e2274efc5ac64e576287918dc626f250d45cf6f
Maudenn <maude_bel@hotmail.fr>
2014-04-04 21:44:05 +0200

    soumission 1

---
+++ TrialRound/src/image/Parser.java
@@ -0,0 +1,68 @@
+package image;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parser {
+
+	public static void main(String[] args){
+		try{
+			InputStream ips=new FileInputStream(args[0]); 
+			boolean[][] tab = parse(ips);
+			List<String> commandes = new ArrayList<>();
+			creerFichier("resultat.txt", creerCommandes(tab));
+		}		
+		catch (Exception e){
+			System.out.println(e.toString());
+		}
+	}
+	
+	public static boolean[][] parse(InputStream in) throws IOException{
+		InputStreamReader ipsr=new InputStreamReader(in);
+		BufferedReader br=new BufferedReader(ipsr);
+		String ligne = br.readLine();
+		String[] s = ligne.split(" ");
+		int hauteur = Integer.parseInt(s[0]);
+		int largeur = Integer.parseInt(s[1]);
+		boolean tab[][] = new boolean[hauteur][largeur];
+		int i = 0;
+		while ((ligne=br.readLine())!=null){
+			for(int j=0; j<largeur; j++){
+				if(ligne.charAt(j)=='#') tab[i][j] = true;
+				else tab[i][j] = false;
+			}
+			i++;
+		}
+		br.close(); 
+		return tab;
+	}
+	
+	public static void creerFichier(String nom, List<String> commandes){
+		try {
+			FileWriter fw = new FileWriter (nom);
+			BufferedWriter bw = new BufferedWriter (fw);
+			PrintWriter fichierSortie = new PrintWriter (bw);
+			fichierSortie.println(commandes.size());
+			for(String s : commandes){
+				fichierSortie.println(s);
+			}
+			fichierSortie.close();
+		}
+		catch (Exception e){
+			System.out.println(e.toString());
+		}	
+	}
+	
+	public static List<String> creerCommandes (boolean[][] tab){
+		
+		return null;
+	}
+}
